c0382c607d45d4a28a3fa0638204605b1868a05a
Pouya Kary <k@karyfoundation.org>
2016-12-31 14:43:02 +0330

    Special builder for Braver

---
+++ tools/build-braver.sh
@@ -0,0 +1,35 @@
+
+#
+# ─── GOING UP ───────────────────────────────────────────────────────────────────
+#
+
+    pwd
+
+#
+# ─── PACKAGE THE WHOLE THING ────────────────────────────────────────────────────
+#
+
+    export CHANNEL=dev
+    npm run build-package
+
+#
+# ─── COPY THE INFO PLIST ────────────────────────────────────────────────────────
+#
+
+    yes | cp -rf ./braver/info.plist ./Brave-darwin-x64/Brave.app/Contents/info.plist
+
+#
+# ─── CHANGING THE ICON ──────────────────────────────────────────────────────────
+#
+
+    yes | cp -rf ./braver/electron.icns ./Brave-darwin-x64/Brave.app/Contents/Resources/electron.icns
+
+#
+# ─── RENAME BUNDLE ──────────────────────────────────────────────────────────────
+#
+
+    mv ./Brave-darwin-x64/Brave.app ./Brave-darwin-x64/Braver.app
+
+# ────────────────────────────────────────────────────────────────────────────────
+
+
